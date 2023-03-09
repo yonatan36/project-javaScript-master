ffe5ba247e0e7dc28d32ae9539b68056feb40171
yonatan taub <yonatantaub36@gmail.com>
2023-03-09 14:24:55 +0200

    end comit!

--- project-javaScript-master/src/components/Navbar.js
+++ project-javaScript-master/src/components/Navbar.js
@@ -5,6 +5,8 @@ import CheckIfConnected from "../utils/checkIfConected.js";
 const navBeforeLogin = document.getElementById("navBeforeLogin");
 const navAfterLogin = document.getElementById("navAfterLogin");
 const navHomeLink = document.getElementById("nav-home-link");
+const aboutus = document.getElementById("about-us");
+const homepage = document.getElementById("home-page");
 
   let isAdmin;
   let isConnected;
@@ -18,6 +20,9 @@ const NavbarSettings = () => {
     navBeforeLogin.classList.add("d-none");
     navAfterLogin.classList.remove("d-none");
     navHomeLink.classList.remove("d-none")
+    aboutus.classList.add("d-none");
+    homepage.classList.remove("d-none")
+
  
   }
 };
